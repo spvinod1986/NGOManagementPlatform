0e2a08cf964b927ccdc3a6fe40e2ec828e4877c8
Vinod SP <vinodsp@Vinods-MacBook-Pro.local>
2019-06-23 11:34:47 -0400

    Added unit tests project for application layer

---
+++ NonProfit.Application.Tests/TestSetting.cs
@@ -0,0 +1,8 @@
+namespace NonProfit.Application.Tests
+{
+    public static class TestSetting
+    {
+        public static readonly string ConnectionString = "Server=localhost;Database=NonProfit;User Id=sa;Password=Passw0rd;MultipleActiveResultSets=true";
+
+    }
+}
